7eede6de2fcd89c73fb6e8c4df71f954c121a451
Jose Luis Crisostomo <jl.cs606@gmail.com>
2024-08-08 14:36:24 +0200

    pushing a greeting

--- androidApp/src/main/java/com/example/example/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/example/android/MainActivity.kt
@@ -9,7 +9,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,9 +24,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.example.Greeting
 import com.example.example.android.launch.MainViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +57,7 @@ fun App() {
                 Text(greeting)
                 HorizontalDivider()
             }
+            GreetingView(text = greetings.first())
         }
     }
 
